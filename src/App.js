9be6631fee850f3f6e8ad72d904437fdc561c273
diniji <dina.nijikovsky@gmail.com>
2023-02-22 22:38:14 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { data } from './data';
 import './App.css';
 
+
 function App() {
+
+  const [photo, setPhoto] = useState(0);
+  const {img} = data[photo];
+  console.log(data[photo]);
+
+  const previousPhoto = () => {
+    setPhoto((photo => {
+      photo --;
+      if (photo <0) {
+        photo = data.length -1;
+      }
+      return photo;
+    }))
+  }
+
+  const nextPhoto = () => {
+    setPhoto((photo => {
+      photo ++;
+      if (photo > data.length -1) {
+        photo = 0;
+      }
+      return photo;
+    }))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div className="header">
+        <h1>Grandparents and us 💕</h1>
+      </div>
+
+      <div className="container">
+        <button onClick={previousPhoto} className="btnBack">Back</button>
+        <img src={img} className="pics" width="500px" alt="grandparents and grandchildren" />
+        <button onClick={nextPhoto} className="btnNext">Next</button>
+      </div>
+      
+      <div>
+        <p className="bottom"></p>
+      </div>
+      
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,65 @@
+import image1 from './Photos/image1.jpg';
+import image2 from './Photos/image2.jpg';
+import image3 from './Photos/image3.jpg';
+import image4 from './Photos/image4.jpg';
+import image5 from './Photos/image5.jpg';
+import image6 from './Photos/image6.jpg';
+import image7 from './Photos/image7.jpg';
+import image8 from './Photos/image8.jpg';
+import image9 from './Photos/image9.jpg';
+import image10 from './Photos/image10.jpg';
+import image11 from './Photos/image11.jpg';
+import image12 from './Photos/image12.jpg';
+
+
+export const data = [
+
+    {
+        img: image1
+    },
+
+    {
+        img: image2
+    },
+
+    {
+        img: image3
+    },
+
+    {
+        img: image4
+    },
+
+    {
+        img: image5
+    },
+
+    {
+        img: image6
+    },
+
+    {
+        img: image7
+    },
+
+    {
+        img: image8
+    },
+
+    {
+        img: image9
+    },
+
+    {
+        img: image10
+    },
+
+    {
+        img: image11
+    },
+
+    {
+        img: image12
+    }
+
+]
